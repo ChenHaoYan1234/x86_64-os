1c3d49e6873ca5b6df82b85968248b53d93792c4
ChenHaoYan1234 <chanhaoyan@126.com>
2026-08-05 10:19:43 +0800

    Add initial boot.asm and loader.asm

---
+++ src/loader.asm
@@ -0,0 +1,25 @@
+    org 10000h
+
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+; display on screen: Start Loader
+    mov ax, 1301h
+    mov bx, 000fh
+    mov cx, 12
+    mov dx, 0200h
+    push ax
+    mov ax, ds
+    mov es, ax
+    pop ax
+    mov bp, StartLoaderMessage
+    int 10h
+
+    jmp $
+
+; messages
+StartLoaderMessage: db "Start Loader"
